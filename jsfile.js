a1fe409a912563ae23df1a10259121efbf972aa7
omer <omersorgun34@hotmail.com>
2022-07-04 23:23:56 +0300

    Fat arrow

---
+++ jsfile.js
@@ -0,0 +1,12 @@
+function hello(name){
+    console.log("Merhaba "+name);
+}
+hello("javascript")
+
+const helloFunc=(name)=>{
+    console.log("helloFuncV2");
+}
+const helloFunc2=(name,surname)=>{
+    console.log("hello "+name+surname);
+}
+helloFunc2("omer","yusuf")
